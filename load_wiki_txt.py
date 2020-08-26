6cd422afefed0206f5ede28915ae7aa0f8cb0faa
savoy1211 <ryanlee250@gmail.com>
2020-08-25 18:07:52 -0700

    update load_wiki_txt.py and main_trains

--- load_wiki_txt.py
+++ load_wiki_txt.py
@@ -2,96 +2,103 @@ import os
 import requests
 # from download_local_gutenberg_texts import *
 
-# turkish texts
-turkish_100 =''
-with open('lm_corpus_turkish_100_small', 'r', encoding="utf-8") as current_file:
+# hungarian texts
+hungarian_100 =''
+with open('gutenberg_corpus_hungarian_100', 'r', encoding="utf-8") as current_file:
     text = current_file.read().encode('utf-8')[1:]
-    turkish_100 = text.decode('utf-8', 'ignore')
+    hungarian_100 = text.decode('utf-8', 'ignore')
 
-turkish_90 =''
-with open('lm_corpus_turkish_90_small', 'r', encoding="utf-8") as current_file:
+hungarian_90 =''
+with open('gutenberg_corpus_hungarian_90', 'r', encoding="utf-8") as current_file:
     text = current_file.read().encode('utf-8')[1:]
-    turkish_90 = text.decode('utf-8', 'ignore')
+    hungarian_90 = text.decode('utf-8', 'ignore')
 
-turkish_10 =''
-with open('lm_corpus_turkish_10_small', 'r', encoding="utf-8") as current_file:
+hungarian_10 =''
+with open('gutenberg_corpus_hungarian_10', 'r', encoding="utf-8") as current_file:
     text = current_file.read().encode('utf-8')[1:]
-    turkish_10 = text.decode('utf-8', 'ignore')
+    hungarian_10 = text.decode('utf-8', 'ignore')
 
-print("turkish texts loaded!")
+print("hungarian texts loaded!")
 
-# language = "turkish"
-# # # turkish texts
-# turkish_100 =''
-# with open('lm_corpus_turkish_100', 'r', encoding="utf-8") as current_file:
+
+
+
+
+
+
+
+# language = "hungarian"
+# # # hungarian texts
+# hungarian_100 =''
+# with open('lm_corpus_hungarian_100', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_100 = text.decode('utf-8', 'ignore')
-#     turkish_100 = turkish_100[:int(len(turkish_100)/3)]
+#     hungarian_100 = text.decode('utf-8', 'ignore')
+#     hungarian_100 = hungarian_100[:int(len(hungarian_100)/3)]
 # file = open("lm_corpus_"+language+"_100_small", "w")
-# file.write(turkish_100)
+# file.write(hungarian_100)
 
 
-# turkish_90 =''
-# with open('lm_corpus_turkish_90', 'r', encoding="utf-8") as current_file:
+# hungarian_90 =''
+# with open('lm_corpus_hungarian_90', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_90 = text.decode('utf-8', 'ignore')
-#     turkish_90 = turkish_90[:int(len(turkish_90)/3)]
-#     # turkish_90 = turkish_90[:int(len(turkish_90))]
+#     hungarian_90 = text.decode('utf-8', 'ignore')
+#     hungarian_90 = hungarian_90[:int(len(hungarian_90)/3)]
+#     # hungarian_90 = hungarian_90[:int(len(hungarian_90))]
 # file = open("lm_corpus_"+language+"_90_small", "w")
-# file.write(turkish_90)
+# file.write(hungarian_90)
 
-# turkish_10 =''
-# with open('lm_corpus_turkish_10', 'r', encoding="utf-8") as current_file:
+# hungarian_10 =''
+# with open('lm_corpus_hungarian_10', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_10 = text.decode('utf-8', 'ignore')
-#     turkish_10 = turkish_10[:int(len(turkish_10)/3)]
-#     # turkish_10 = turkish_10[:int(len(turkish_10))]
+#     hungarian_10 = text.decode('utf-8', 'ignore')
+#     hungarian_10 = hungarian_10[:int(len(hungarian_10)/3)]
+#     # hungarian_10 = hungarian_10[:int(len(hungarian_10))]
 # file = open("lm_corpus_"+language+"_10_small", "w")
-# file.write(turkish_10)
-# turkish texts
-# turkish_100 =''
-# with open('lm_corpus_turkish_100_small', 'r', encoding="utf-8") as current_file:
+# file.write(hungarian_10)
+# hungarian texts
+# hungarian_100 =''
+# with open('lm_corpus_hungarian_100_small', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_100 = text.decode('utf-8', 'ignore')
+#     hungarian_100 = text.decode('utf-8', 'ignore')
 
-# turkish_90 =''
-# with open('lm_corpus_turkish_90_small', 'r', encoding="utf-8") as current_file:
+# hungarian_90 =''
+# with open('lm_corpus_hungarian_90_small', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_90 = text.decode('utf-8', 'ignore')
+#     hungarian_90 = text.decode('utf-8', 'ignore')
 
-# turkish_10 =''
-# with open('lm_corpus_turkish_10_small', 'r', encoding="utf-8") as current_file:
+# hungarian_10 =''
+# with open('lm_corpus_hungarian_10_small', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_10 = text.decode('utf-8', 'ignore')
+#     hungarian_10 = text.decode('utf-8', 'ignore')
 
-# print("turkish texts loaded!")
+# print("hungarian texts loaded!")
 
-# language = "turkish"
-# # # turkish texts
-# turkish_100 =''
-# with open('lm_corpus_turkish_100', 'r', encoding="utf-8") as current_file:
+# language = "hungarian"
+# # # hungarian texts
+# hungarian_100 =''
+# with open('lm_corpus_hungarian_100', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_100 = text.decode('utf-8', 'ignore')
-#     turkish_100 = turkish_100[:int(len(turkish_100)/3)]
+#     hungarian_100 = text.decode('utf-8', 'ignore')
+#     hungarian_100 = hungarian_100[:int(len(hungarian_100)/3)]
 # file = open("lm_corpus_"+language+"_100_small", "w")
-# file.write(turkish_100)
+# file.write(hungarian_100)
 
 
-# turkish_90 =''
-# with open('lm_corpus_turkish_90', 'r', encoding="utf-8") as current_file:
+# hungarian_90 =''
+# with open('lm_corpus_hungarian_90', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_90 = text.decode('utf-8', 'ignore')
-#     turkish_90 = turkish_90[:int(len(turkish_90)/3)]
-#     # turkish_90 = turkish_90[:int(len(turkish_90))]
+#     hungarian_90 = text.decode('utf-8', 'ignore')
+#     hungarian_90 = hungarian_90[:int(len(hungarian_90)/3)]
+#     # hungarian_90 = hungarian_90[:int(len(hungarian_90))]
 # file = open("lm_corpus_"+language+"_90_small", "w")
-# file.write(turkish_90)
+# file.write(hungarian_90)
 
-# turkish_10 =''
-# with open('lm_corpus_turkish_10', 'r', encoding="utf-8") as current_file:
+# hungarian_10 =''
+# with open('lm_corpus_hungarian_10', 'r', encoding="utf-8") as current_file:
 #     text = current_file.read().encode('utf-8')[1:]
-#     turkish_10 = text.decode('utf-8', 'ignore')
-#     turkish_10 = turkish_10[:int(len(turkish_10)/3)]
-#     # turkish_10 = turkish_10[:int(len(turkish_10))]
+#     hungarian_10 = text.decode('utf-8', 'ignore')
+#     hungarian_10 = hungarian_10[:int(len(hungarian_10)/3)]
+#     # hungarian_10 = hungarian_10[:int(len(hungarian_10))]
 # file = open("lm_corpus_"+language+"_10_small", "w")
-# file.write(turkish_10)
+# file.write(hungarian_10)
 
